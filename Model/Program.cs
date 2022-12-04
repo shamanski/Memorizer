edeba4b56b63ab284d82869e52a863319a39ef20
shamanski <naumenka86@gmail.com>
2022-12-04 19:18:37 +0100

    db maker class extraction

--- Model/Program.cs
+++ Model/Program.cs
@@ -48,45 +48,11 @@ namespace ReversoApi
     static class Program
     {
 
-        private static readonly string txtfile = "c:\\plot\\3.txt";
-        private static readonly string outputTxtfile = "c:\\plot\\2.txt";
-        private static readonly string xmlfile = "d:\\words.xml";
-
-        static void Migration()
-        {
-            var translates = ReadTranslates();
-            var dbmodel = new List<Word>();
-            foreach (var word in translates)
-            {
-                if (!word.Error && word.Success)
-                {
-                    var w = new Word
-                    {
-                        Text = word.Sources[0].DisplaySource
-                    };
-                    var items = (from translate in word.Sources[0].Translations
-                                 select new Translate
-                                 {
-                                     Text = translate.Translation
-                                 }).ToList();
-                    w.Translates = new List<Translate>();
-                    w.Translates.AddRange(items);
-                    dbmodel.Add(w);
-                }
-                else Console.WriteLine($"{word.Sources[0].DisplaySource }");
-            }
-            using var db = new ReversoConsole.Controller.WebAppContext();
-            db.AddRange(dbmodel);
-            db.SaveChanges();
-            Console.WriteLine("Объекты успешно сохранены");
-
-        }
-
         //Run this if you want console application instead of webserver
         static async Task Main(string[] args)
         {
 
-            Clear();
+            DatabaseMakerAssistant.Clear();
             Console.WriteLine("EnterName");
             var name = Console.ReadLine();
             //var userController = new UserController();                      
@@ -101,49 +67,6 @@ namespace ReversoApi
                 Console.Write("Enter command: >");
                 //await command.Execute(userController, Console.ReadLine());
             }
-        }
-
-        static void Clear()
-        {
-            var res = File.ReadLines(txtfile);
-            res = res.Select(x => x.Split(" ", 10).First()).ToList();
-            File.WriteAllLines(outputTxtfile, res);
-
-        }
-
-        static void Save()
-        {
-            var res = File.ReadLines(txtfile)
-                           .Where(l => l.Length > 0)
-                           .Select(x => x.Split(new[] { ')' }, StringSplitOptions.RemoveEmptyEntries))
-                           .Select(i => new WordDescription(i[0], i[1]))
-                           .ToList();
-            Console.WriteLine(res[0].Word);
-            var wds = new NWords(res);
-            XmlSerializer formatter = new XmlSerializer(typeof(NWords));
-            using FileStream fs = new FileStream(xmlfile, FileMode.OpenOrCreate);
-            formatter.Serialize(fs, wds);
-
-            Console.WriteLine("Список сериализован");
-        }
-
-        static void SaveTranslates(List<TranslatedResponse> list)
-        {
-            XmlSerializer formatter = new XmlSerializer(typeof(List<TranslatedResponse>));
-            using FileStream fs = new FileStream(xmlfile, FileMode.OpenOrCreate);
-            formatter.Serialize(fs, list);
-
-            Console.WriteLine("Список сериализован");
-        }
-        static List<TranslatedResponse> ReadTranslates()
-        {
-
-            XmlSerializer formatter = new XmlSerializer(typeof(List<TranslatedResponse>));
-            using FileStream fs = new FileStream(xmlfile, FileMode.OpenOrCreate);
-            var wds = (List<TranslatedResponse>)formatter.Deserialize(fs);
-            Console.WriteLine("Список десериализован");
-            return wds;
-
         }
 
         static List<TranslatedResponse> Request(NWords words)
